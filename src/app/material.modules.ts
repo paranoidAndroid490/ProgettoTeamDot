1b506cfdb87b56abb23fea90331f0f47fa900258
Nick <nick.locorotondo@gmail.com>
2022-06-20 11:27:00 +0200

    Aggiunto Form modifica yatch

--- src/app/material.modules.ts
+++ src/app/material.modules.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  imports: [MatCardModule],
-  exports: [MatCardModule]
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  exports: [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule]
 })
 
 export class MaterialModule{
